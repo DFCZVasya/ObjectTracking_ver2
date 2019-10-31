607cb537d48abdef4055db43eeb367031b6b8846
Василий Лопаткин <loopatkin.2002@outlook.com>
2019-10-31 11:56:43 +0700

    end?

--- main.py
+++ main.py
@@ -18,10 +18,6 @@ files = glob.glob('output/*.png')
 for f in files:
 	os.remove(f)
 
-#from sort import *
-#tracker = Sort()
-#memory = {}
-
 #resolution = input('Enter Resolution: ')
 resolution = '4k'
 if resolution == '4k':
@@ -49,39 +45,11 @@ ap.add_argument("-o", "--output", required=True,
 #	help="threshold when applyong non-maxima suppression")
 args = vars(ap.parse_args())
 
-# Return true if line segments AB and CD intersect
-#def intersect(A,B,C,D):
-#	return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
-
-#def ccw(A,B,C):
-#	return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
-
-# load the COCO class labels our YOLO model was trained on
-#labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
-#LABELS = open(labelsPath).read().strip().split("\n")
-
-# initialize a list of colors to represent each possible class label
-#np.random.seed(42)
-#COLORS = np.random.randint(0, 255, size=(200, 3),
-#	dtype="uint8")
-
-# derive the paths to the YOLO weights and model configuration
-#weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
-#configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
-
-# load our YOLO object detector trained on COCO dataset (80 classes)
-# and determine only the *output* layer names that we need from YOLO
-#print("[INFO] loading YOLO from disk...")
-#net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
-#ln = net.getLayerNames()
-#ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
-# initialize the video stream, pointer to output video file, and
 # frame dimensions
 vs = cv2.VideoCapture(args["input"])
 writer = None
 (W, H) = (None, None)
-
+# load our YOLO object detector trained on COCO dataset (80 classes)
 yolo = YOLO()
 
 frameIndex = 0
@@ -119,77 +87,7 @@ while True:
 	#image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB);
 
 	outBoxes = yolo.detect_image(image)
-	# construct a blob from the input frame and then perform a forward
-	# pass of the YOLO object detector, giving us our bounding boxes
-	# and associated probabilities
-	#blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-	#	swapRB=True, crop=False)
-	#net.setInput(blob)
-	###################################################################
-	#layerOutputs = net.forward(ln)
-	####################################################################
-	# initialize our lists of detected bounding boxes, confidences,
-	# and class IDs, respectively
-	#boxes = []
-	#confidences = []
-	#classIDs = []
-
-	# loop over each of the layer outputs
-	#for output in layerOutputs:
-		# loop over each of the detections
-		#for detection in output:
-			# extract the class ID and confidence (i.e., probability)
-			# of the current object detection
-			#scores = detection[5:]
-			#classID = np.argmax(scores)
-			#confidence = scores[classID]
-
-			# filter out weak predictions by ensuring the detected
-			# probability is greater than the minimum probability
-			#if confidence > args["confidence"]:
-				# scale the bounding box coordinates back relative to
-				# the size of the image, keeping in mind that YOLO
-				# actually returns the center (x, y)-coordinates of
-				# the bounding box followed by the boxes' width and
-				# height
-				#box = detection[0:4] * np.array([W, H, W, H])
-				#(centerX, centerY, width, height) = box.astype("int")
-
-				# use the center (x, y)-coordinates to derive the top
-				# and and left corner of the bounding box
-				#x = int(centerX - (width / 2))
-				#y = int(centerY - (height / 2))
-
-				# update our list of bounding box coordinates,
-				# confidences, and class IDs
-				#boxes.append([x, y, int(width), int(height)])
-				#confidences.append(float(confidence))
-				#classIDs.append(classID)
-
-	# apply non-maxima suppression to suppress weak, overlapping
-	# bounding boxes
-	#idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["threshold"])
-
-	#dets = []
-	#if len(idxs) > 0:
-		# loop over the indexes we are keeping
-		#for i in idxs.flatten():
-		#	(x, y) = (boxes[i][0], boxes[i][1])
-		#	(w, h) = (boxes[i][2], boxes[i][3])
-		#	dets.append([x, y, x+w, y+h, confidences[i]])
-
-	#np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
-	#dets = np.asarray(dets)
-	#tracks = tracker.update(dets)
-
-	#boxes = []
-	#indexIDs = []
-	#c = []
 	bboxes = []
-
-	#for track in tracks:
-	#	boxes.append([track[0], track[1], track[2], track[3]])
-	#	indexIDs.append(int(track[4]))
 
 	if len(outBoxes) > 0:
 		#i = int(0)
@@ -263,7 +161,7 @@ while True:
 	if resolution == '4k':
 		text = str(len(allObjects)) + ' ' + str(counter)
 		font = cv2.FONT_HERSHEY_SIMPLEX
-		cv2.putText(frame, str(len(allObjects)), (50,50), font, 2, (243, 132, 68), 4, cv2.LINE_AA)
+		#cv2.putText(frame, str(len(allObjects)), (50,50), font, 2, (243, 132, 68), 4, cv2.LINE_AA)
 		cv2.putText(frame, text, (100,200), font, 2, (0, 0, 255), 4, cv2.LINE_AA)
 
 	cv2.imwrite("output/frame-{}.png".format(frameIndex), frame)
