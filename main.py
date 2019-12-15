d1c461e51d873e5b8e67890a6aa17984395bfdc7
DFCZVasya <lopatkin.vasya2017@yandex.ru>
2019-12-15 14:09:19 +0700

    add tiny-yolo

--- main.py
+++ main.py
@@ -90,8 +90,8 @@ writer = None
 (W, H) = (None, None)
 # load our YOLO object detector trained on COCO dataset (80 classes)
 yolo = YOLO(**vars(FLAGS))
-print(FLAGS.anchors)
-print(FLAGS.model)
+#print(FLAGS.anchors)
+#print(FLAGS.model)
 #yolo.anchors = args["anchors"]
 #yolo.model_path = args["model"]
 
